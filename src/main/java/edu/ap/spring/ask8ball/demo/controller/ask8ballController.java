cba5ca1c0ea2347093e62a97097ca84379383130
HaliimaR <rahimi.halima17@gmail.com>
2019-09-04 15:20:40 +0200

    random answer

--- src/main/java/edu/ap/spring/ask8ball/demo/controller/ask8ballController.java
+++ src/main/java/edu/ap/spring/ask8ball/demo/controller/ask8ballController.java
@@ -1,7 +1,8 @@
 package edu.ap.spring.ask8ball.demo.controller;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-
+import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,15 +24,24 @@ public class ask8ballController {
     }
     @PostMapping("/addquestion")
     public String question(@RequestParam("question") String question){
+        Random rand = new Random();
+        String[] answers = new String[]{"It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.","Most likely.", "Outlook good.", "Yes."};
+        
         ArrayList<String> questions = new ArrayList<>();
         String zelfdevraag = question;
+        String answer = "";
         for (String a : this.service.keys("question:" + question + ":*")) {
             questions.add(this.service.getKey(a));
         }
         if (questions.contains(zelfdevraag)) { // countains loopt door u array
             System.out.println("oeps die bestaat wel");
         }else{
-            this.service.setKey("question:" + question + ":" + answer);
+            int max = rand.nextInt(answers.length-1);
+            int min =0;
+            int number = max - min+1;
+            int rnd = (int)((Math.random()*number));
+            answer = answers[(int) Math.floor(rnd)];
+            this.service.setKey("question:" + question + ":" + answer,"");
             System.out.println("vraag word toegevoegd");
         }
         return "redirect:/addquestion";
